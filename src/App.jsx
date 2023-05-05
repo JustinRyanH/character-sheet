8e37873dc94c7e38a7db07100318e5710d4cbb87
Justin Hurstwright <jhurstwright@mavenlink.com>
2023-05-05 14:32:38 -0500

    ref: Extract method

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,42 @@
 import { Grid, TextInput } from "@mantine/core";
 
-function App() {
-  return (
-    <>
-    <Grid>
+function getGrid() {
+    return <Grid>
         <Grid.Col span={6}>
-            <TextInput styles={{  wrapper: { flexGrow: '1' } }} label="Character Name" />
+            <TextInput styles={{ wrapper: { flexGrow: '1' } }} label="Character Name"/>
         </Grid.Col>
         <Grid.Col span={6}>
             <Grid gutter="xs">
                 <Grid.Col span={2}>
-                    <TextInput label="Class" />
+                    <TextInput label="Class"/>
                 </Grid.Col>
                 <Grid.Col span={2}>
-                    <TextInput label="Background" />
+                    <TextInput label="Background"/>
                 </Grid.Col>
                 <Grid.Col span={4}>
-                    <TextInput label="Level" />
+                    <TextInput label="Level"/>
                 </Grid.Col>
                 <Grid.Col span={4}>
-                    <TextInput label="Player Name" />
+                    <TextInput label="Player Name"/>
                 </Grid.Col>
                 <Grid.Col span={4}>
-                    <TextInput label="Race" />
+                    <TextInput label="Race"/>
                 </Grid.Col>
                 <Grid.Col span={4}>
-                    <TextInput label="Alignment" />
+                    <TextInput label="Alignment"/>
                 </Grid.Col>
                 <Grid.Col span={4}>
-                    <TextInput label="Experience Points" />
+                    <TextInput label="Experience Points"/>
                 </Grid.Col>
             </Grid>
         </Grid.Col>
-    </Grid>
+    </Grid>;
+}
+
+function App() {
+  return (
+    <>
+        {getGrid()}
     </>
   )
 }
