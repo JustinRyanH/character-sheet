f3b687e4d317d0011adf0c0f1c4cee12532643f4
Justin Hurstwright <jhurstwright@mavenlink.com>
2023-05-05 14:21:48 -0500

    chore: Remove index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { MantineProvider } from "@mantine/core";
 
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
