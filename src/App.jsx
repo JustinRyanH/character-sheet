6c7a8acb477639341d1f2571e5a2b4e003d18517
Justin Hurstwright <jhurstwright@mavenlink.com>
2023-05-05 15:32:27 -0500

    ref: Extract method

--- src/App.jsx
+++ src/App.jsx
@@ -33,53 +33,57 @@ function CharacterHeader() {
     </Grid>;
 }
 
+function getStack() {
+    return <Stack aria-labelledby="attributes">
+        <TextInput label="Strength"/>
+        <TextInput label="Dexterity"/>
+        <TextInput label="Constitution"/>
+        <TextInput label="Intelligence"/>
+        <TextInput label="Wisdom"/>
+        <TextInput label="Charisma"/>
+        <Header level={2} id="attributes">Attributes</Header>
+    </Stack>;
+}
+
 function App() {
   return (
     <>
         <Stack>
-            <CharacterHeader />
+            <CharacterHeader/>
             <Grid>
                 <Grid.Col span={4}>
                     <Group noWrap>
-                        <Stack aria-labelledby="attributes">
-                            <TextInput label="Strength" />
-                            <TextInput label="Dexterity" />
-                            <TextInput label="Constitution" />
-                            <TextInput label="Intelligence" />
-                            <TextInput label="Wisdom" />
-                            <TextInput label="Charisma" />
-                            <Header level={2} id="attributes">Attributes</Header>
-                        </Stack>
+                        {getStack()}
                         <Stack>
-                            <TextInput label="Proficiency Bonus" />
+                            <TextInput label="Proficiency Bonus"/>
                             <Stack aria-labelledby="saving-throws">
-                                <TextInput size="xs" label="Strength" />
-                                <TextInput size="xs" label="Dexterity" />
-                                <TextInput size="xs" label="Constitution" />
-                                <TextInput size="xs" label="Intelligence" />
-                                <TextInput size="xs" label="Wisdom" />
-                                <TextInput size="xs" label="Charisma" />
+                                <TextInput size="xs" label="Strength"/>
+                                <TextInput size="xs" label="Dexterity"/>
+                                <TextInput size="xs" label="Constitution"/>
+                                <TextInput size="xs" label="Intelligence"/>
+                                <TextInput size="xs" label="Wisdom"/>
+                                <TextInput size="xs" label="Charisma"/>
                                 <Header level={2} id="saving-throws">Saving Throws</Header>
                             </Stack>
                             <Stack aria-labelledby="skills">
-                                <TextInput size="xs" label="Acrobatics" />
-                                <TextInput size="xs" label="Animal Handling" />
-                                <TextInput size="xs" label="Arcana" />
-                                <TextInput size="xs" label="Athletics" />
-                                <TextInput size="xs" label="Deception" />
-                                <TextInput size="xs" label="History" />
-                                <TextInput size="xs" label="Insight" />
-                                <TextInput size="xs" label="Intimidation" />
-                                <TextInput size="xs" label="Investigation" />
-                                <TextInput size="xs" label="Medicine" />
-                                <TextInput size="xs" label="Nature" />
-                                <TextInput size="xs" label="Perception" />
-                                <TextInput size="xs" label="Performance" />
-                                <TextInput size="xs" label="Persuasion" />
-                                <TextInput size="xs" label="Religion" />
-                                <TextInput size="xs" label="Sleight of Hand" />
-                                <TextInput size="xs" label="Stealth" />
-                                <TextInput size="xs" label="Survival" />
+                                <TextInput size="xs" label="Acrobatics"/>
+                                <TextInput size="xs" label="Animal Handling"/>
+                                <TextInput size="xs" label="Arcana"/>
+                                <TextInput size="xs" label="Athletics"/>
+                                <TextInput size="xs" label="Deception"/>
+                                <TextInput size="xs" label="History"/>
+                                <TextInput size="xs" label="Insight"/>
+                                <TextInput size="xs" label="Intimidation"/>
+                                <TextInput size="xs" label="Investigation"/>
+                                <TextInput size="xs" label="Medicine"/>
+                                <TextInput size="xs" label="Nature"/>
+                                <TextInput size="xs" label="Perception"/>
+                                <TextInput size="xs" label="Performance"/>
+                                <TextInput size="xs" label="Persuasion"/>
+                                <TextInput size="xs" label="Religion"/>
+                                <TextInput size="xs" label="Sleight of Hand"/>
+                                <TextInput size="xs" label="Stealth"/>
+                                <TextInput size="xs" label="Survival"/>
                                 <Header level={2} id="skills">Skills</Header>
                             </Stack>
                         </Stack>
@@ -88,14 +92,14 @@ function App() {
                 <Grid.Col span={4}>
                     <Stack aria-labelledby="combat-stats">
                         <Group position="apart" noWrap>
-                            <TextInput label="Armor Class" />
-                            <TextInput label="Initiative" />
-                            <TextInput label="Speed" />
+                            <TextInput label="Armor Class"/>
+                            <TextInput label="Initiative"/>
+                            <TextInput label="Speed"/>
                         </Group>
-                        <Stack >
-                            <TextInput label="Hit Point Maximum" />
-                            <TextInput label="Current Hit Points" />
-                            <TextInput label="Temporary Hit Points" />
+                        <Stack>
+                            <TextInput label="Hit Point Maximum"/>
+                            <TextInput label="Current Hit Points"/>
+                            <TextInput label="Temporary Hit Points"/>
                         </Stack>
                         <Group>
                             <div aria-labelledby="hit-dice">
@@ -110,11 +114,11 @@ function App() {
                     <Stack aria-labelledby="attacks-and-spells">
                         <Table>
                             <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Atk Bonus</th>
-                                    <th>Damage/Type</th>
-                                </tr>
+                            <tr>
+                                <th>Name</th>
+                                <th>Atk Bonus</th>
+                                <th>Damage/Type</th>
+                            </tr>
                             </thead>
                         </Table>
                         <Header id="attacks-and-spells" level={2}>Attacks and Spellcasting</Header>
