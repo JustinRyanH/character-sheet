32d19e5de226f4ae70e19f27534b3bd106280a47
Justin Hurstwright <jhurstwright@mavenlink.com>
2023-05-05 14:23:04 -0500

    design: Layout Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import { TextInput } from "@mantine/core";
+import { Grid, Group, TextInput } from "@mantine/core";
 
 function App() {
   return (
     <>
-        <TextInput label="Character Name" />
-        <TextInput label="Class" />
-        <TextInput label="Level" />
-        <TextInput label="Background" />
-        <TextInput label="Player Name" />
-        <TextInput label="Race" />
-        <TextInput label="Alignment" />
-        <TextInput label="Experience Points" />
+        <Group>
+            <TextInput label="Character Name" />
+            <Grid>
+                <Grid.Col span={2}>
+                    <TextInput label="Class" />
+                </Grid.Col>
+                <Grid.Col span={2}>
+                    <TextInput label="Background" />
+                </Grid.Col>
+                <Grid.Col span={4}>
+                    <TextInput label="Level" />
+                </Grid.Col>
+                <Grid.Col span={4}>
+                    <TextInput label="Player Name" />
+                </Grid.Col>
+                <Grid.Col span={4}>
+                    <TextInput label="Race" />
+                </Grid.Col>
+                <Grid.Col span={4}>
+                    <TextInput label="Alignment" />
+                </Grid.Col>
+                <Grid.Col span={4}>
+                    <TextInput label="Experience Points" />
+                </Grid.Col>
+            </Grid>
+        </Group>
     </>
   )
 }
