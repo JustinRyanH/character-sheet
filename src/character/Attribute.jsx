b015b21c4392cdb7af961b0ebd826ba43f044cd4
Justin Hurstwright <jhurstwright@mavenlink.com>
2023-06-09 12:16:22 -0500

    ref: Documentation

--- src/character/Attribute.jsx
+++ src/character/Attribute.jsx
@@ -1,6 +1,10 @@
 // eslint-disable-next-line react/prop-types
 import { Group, Stack, Text, TextInput } from "@mantine/core";
 
+
+// We have Value
+// That value results in a modifier
+// The modifier is derived from `(ability / 2) -5`
 export function Attribute({ label }) {
     return <Stack>
         <TextInput label={label}/>
