ae17f4edc4943bbf438cc578a1b243048822ccd4
Justin Hurstwright <jhurstwright@mavenlink.com>
2023-06-09 12:40:17 -0500

    feat: Update the Value

--- src/character/Attribute.jsx
+++ src/character/Attribute.jsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line react/prop-types
+import React from 'react';
 import { Group, NumberInput, Stack, Text } from "@mantine/core";
 
 
@@ -6,13 +6,14 @@ import { Group, NumberInput, Stack, Text } from "@mantine/core";
 // That value results in a modifier
 // The modifier is derived from `(ability / 2) -5`
 // eslint-disable-next-line react/prop-types
-export function Attribute({ label, value }) {
+export function Attribute({ label, value: initialValue }) {
+    const [value, setValue] = React.useState(initialValue);
     const modifier = Math.floor((value / 2) - 5);
     const sign = Math.sign(modifier);
     let displaySign = sign === 1 ? '+' : '-';
     if (sign === 0) displaySign = ''
     return <Stack>
-        <NumberInput label={label} defaultValue={value} />
+        <NumberInput label={label} value={value} onChange={(value) => setValue(value)} />
         <Group position="center">
             <Text aria-label={`${label} Modifier`}>{displaySign}{Math.abs(modifier)}</Text>
         </Group>
--- src/character/Attributes.jsx
+++ src/character/Attributes.jsx
@@ -5,7 +5,7 @@ import { Attribute } from "./Attribute.jsx";
 export function Attributes() {
     return <Stack aria-labelledby="attributes">
         <Header level={2} id="attributes">Attributes</Header>
-        <Attribute label="Strength"/>
+        <Attribute value={10} label="Strength"/>
         <TextInput label="Dexterity"/>
         <TextInput label="Constitution"/>
         <TextInput label="Intelligence"/>
