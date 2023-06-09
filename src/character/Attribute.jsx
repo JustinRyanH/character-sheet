462f4b1746d6d3e88dee4418515a7fb2ead832bf
Justin Hurstwright <jhurstwright@mavenlink.com>
2023-06-09 12:19:13 -0500

    ref: Remove duplication

--- src/character/Attribute.jsx
+++ src/character/Attribute.jsx
@@ -1,14 +1,14 @@
 // eslint-disable-next-line react/prop-types
-import { Group, Stack, Text, TextInput } from "@mantine/core";
+import { Group, NumberInput, Stack, Text } from "@mantine/core";
 
 
 // We have Value
 // That value results in a modifier
 // The modifier is derived from `(ability / 2) -5`
 // eslint-disable-next-line react/prop-types
-export function Attribute({ label }) {
+export function Attribute({ label, value }) {
     return <Stack>
-        <TextInput label={label} defaultValue="10" />
+        <NumberInput label={label} defaultValue={value} />
         <Group position="center">
             <Text aria-label={`${label} Modifier`}>+1</Text>
         </Group>
