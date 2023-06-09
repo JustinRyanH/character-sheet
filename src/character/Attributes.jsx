33794a662bf7beb40f994188dce9ae8a9246f841
Justin Hurstwright <jhurstwright@mavenlink.com>
2023-06-09 12:14:47 -0500

    spike: Add in Modifier

--- src/character/Attributes.jsx
+++ src/character/Attributes.jsx
@@ -1,18 +1,24 @@
-import { Header, Stack, TextInput } from "@mantine/core";
+import { Header, Stack, TextInput, Text, Group } from "@mantine/core";
 
+// eslint-disable-next-line react/prop-types
 function Attribute({ label }) {
-    return <TextInput label="Strength"/>
+    return <Stack>
+        <TextInput label={label} />
+        <Group position="center">
+            <Text aria-label={`${label} Modifier`}>+1</Text>
+        </Group>
+    </Stack>
 }
 
 
 export function Attributes() {
     return <Stack aria-labelledby="attributes">
+        <Header level={2} id="attributes">Attributes</Header>
         <Attribute label="Strength"/>
         <TextInput label="Dexterity"/>
         <TextInput label="Constitution"/>
         <TextInput label="Intelligence"/>
         <TextInput label="Wisdom"/>
         <TextInput label="Charisma"/>
-        <Header level={2} id="attributes">Attributes</Header>
     </Stack>;
 }
